0b09ad0ef1540969f356b5de03a188369c4ab93a
Prerak Varshney <e22cseu0138@bennett.edu.in>
2025-02-13 17:04:06 +0530

    Added error middleware

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express';
+
 import { PORT } from './config/env.js';
+
 import connectToDatabase from './database/mongodb.js';
+
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
@@ -8,13 +13,19 @@ import subscriptionRouter from './routes/subscriptions.routes.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subscriptions tracker API');
+})
 
 app.listen(PORT, async() => {
     await connectToDatabase();
