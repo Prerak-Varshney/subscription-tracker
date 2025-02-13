5c8b4daa3d08cca75e5ccb1763997c9c3b61a6c7
Prerak Varshney <e22cseu0138@bennett.edu.in>
2025-02-13 19:53:45 +0530

    Added arcjet for rate limiting and other security protocols

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import { PORT } from './config/env.js';
 
 import connectToDatabase from './database/mongodb.js';
 
-import errorMiddleware from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
+import errorMiddleware from "./middleware/error.middleware.js";
+import arcjetMiddleware from "./middleware/arcjet.middleware.js";
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
@@ -16,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
